3ab1d126eb06e00151f0aba5062ae8a43958c37f
stclark278 <69922727+stclark278@users.noreply.github.com>
2021-12-02 15:44:10 -0600

    Added error checking and the ability to do non-square matrices.

--- matrix_trans_single.c
+++ matrix_trans_single.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <Windows.h>
+#include <unistd.h>
 
 int matrix [10001][10001];
 int transposedMatrix [10001][10001];
 
 //function fills the matrix with the correct amount of random integers
-int fillMatrix(int rows, int seed)
+void fillMatrix(int rows, int cols, int seed)
 {
     int i, j;
 
@@ -14,38 +16,38 @@ int fillMatrix(int rows, int seed)
 
     for (i = 0; i < rows; i++)
     {
-        for (j = 0; j < rows; j++)
+        for (j = 0; j < cols; j++)
         {
             matrix[i][j] = rand() % 100;
         }
     }
 
-    return 0;
+    return;
 }
 
 //function prints the original matrix for testing purposes
-int printMatrix(int rows)
+void printMatrix(int rows, int cols)
 {
     int i, j;
 
     for (i = 0; i < rows; i++)
     {
-        for (j = 0; j < rows; j++)
+        for (j = 0; j < cols; j++)
         {
             printf("%4d", matrix[i][j]);
         }
         printf("\n\n");
     }
 
-    return 0;
+    return;
 }
 
 //function prints the transposed matrix for testing purposes
-int printTransMatrix(int rows)
+void printTransMatrix(int rows, int cols)
 {
     int i, j;
 
-    for (i = 0; i < rows; i++)
+    for (i = 0; i < cols; i++)
     {
         for (j = 0; j < rows; j++)
         {
@@ -54,47 +56,80 @@ int printTransMatrix(int rows)
         printf("\n\n");
     }
 
-    return 0;
+    return;
 }
 
-//function creates a transposed matrix from the original matrix
-int transposeMatrix(int rows)
+//function creates a transposed matrix from the original matrix, sleep added to slow down the function
+void transposeMatrix(int rows, int cols)
 {
     int i, j;
 
     for (i = 0; i < rows; i++)
     {
-        for (j = 0; j < rows; j++)
+        for (j = 0; j < cols; j++)
         {
-            transposedMatrix[i][j] = matrix[j][i];
-            sleep(transposedMatrix[i][j] / 100);          
+            transposedMatrix[j][i] = matrix[i][j];
+            sleep(transposedMatrix[j][i] / 100);
         }
     }
+    return;
 }
+
+//function checks to see if transpose successfully worked
+int transposeCheck(int rows, int cols)
+{
+    int i, j;
+
+    j = 0;
+
+    for (i = 0; i < rows; i++)
+    {
+        if (transposedMatrix[j][i] != matrix[i][j])
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 
 int main()
 {
     int rows;
+    int cols;
     int goodRows;
+    int goodCols;
     int seed;
     int valid;
     clock_t startTime;
     clock_t endTime;
     double totalTime;
+    int success;
 
     valid = 0;
     goodRows = 0;
+    goodCols = 0;
 
     //asks user for number of rows and a seed to use
     printf("Welcome to Matrix Transposition Single Thread!\n");
     printf("----------------------------------------------\n");
     while (goodRows == 0)
     {
-        printf("Please enter the number of rows for the square matrix. (1-10000)\n");
+        printf("Please enter the number of rows for the matrix. (1-10000)\n");
         scanf("%d", &rows);
         if (rows > 0 && rows < 10001)
         {
             goodRows = 1;
+        }
+    }
+        while (goodCols == 0)
+    {
+        printf("Please enter the number of columns for the matrix. (1-10000)\n");
+        scanf("%d", &cols);
+        if (cols > 0 && cols < 10001)
+        {
+            goodCols = 1;
         }
     }
     printf("Please enter a random seed to test.\n");
@@ -105,8 +140,8 @@ int main()
     // printf("\n");
     // printf("Starting Matrix\n\n");
 
-    fillMatrix(rows, seed);
-    // printMatrix(rows);
+    fillMatrix(rows, cols, seed);
+    // printMatrix(rows, cols);
 
     // printf("\n");
     // printf("----------------------------------------------\n");
@@ -115,14 +150,23 @@ int main()
 
     startTime = clock();
 
-    transposeMatrix(rows);
-    // printTransMatrix(rows);
+    transposeMatrix(rows, cols);
+    // printTransMatrix(rows, cols);
 
     endTime = clock();
 
+    success = transposeCheck(rows, cols);
+
     totalTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
 
-    printf("Matrix Transposed in %.3f seconds\n", totalTime);
+    if (success == 1)
+    {
+        printf("Matrix Successfully Transposed in %.3f seconds\n", totalTime);
+    }
+    else
+    {
+        printf("Matrix Unsuccessfully Transposed in %.3f second\n", totalTime);
+    }
 
     return 0;
 }
